5717cc92af9b4ba8b00211e3ff5cdfaeec1c16a2
lulujal <jaluadhi19@students.unnes.ac.id>
2023-11-19 10:41:22 +0700

    add user login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,15 @@
 const { query } = require('express')
 const {comparePassword} = require('../helpers/bcrypt')
 const {generateToken} = require('../helpers/jwt')
-const {User} = require('../models')
+const {User} = require('../models');
+const user = require('../models/user');
 
 class UserController {
     static register(req, res) {
         try {
             const { full_name, password, gender, email } = req.body;
             User.create({
-                full_name,
+                full_name,   
                 password,
                 gender,
                 email,
@@ -19,10 +20,45 @@ class UserController {
                     full_name: result.full_name,
                     email: result.email,
                     gender: result.gender,
-                    balance: `Rp ${result.balance}`,
+                    balance: `Rp ${result.balance.toLocaleString('id-ID')}`,
                     createdAt: result.createdAt,
                 }
                 res.status(201).json(response);
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json(error);
+        }
+    }
+
+    static login (req, res) {
+        try {
+            const {email, password} = req.body;
+            User.findOne({
+                where: {
+                    email
+                }
+            })
+            .then(user => {
+                if (!user) {
+                    throw {
+                        name: 'user login error',
+                        devMessage: 'user not found',
+                    }
+                }
+                const isCorrect = comparePassword(password, user.password);
+                if (!isCorrect) {
+                    throw {
+                        name: 'user login error',
+                        devMessage: 'wrong password',
+                    }
+                }
+                const payload = {
+                    id: user.id,
+                    email: user.email,
+                }
+                const token = generateToken(payload);
+                res.status(200).json({token});
             })
         } catch (error) {
             console.log(error);
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 const UserController = require('../controllers/usercontroller');
 
 router.post('/users/register', UserController.register);
+router.post('/users/login', UserController.login);
 module.exports = router;
