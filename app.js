fbce7466a0cbb68bcde9aac5e1f81ec3483b3f22
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-23 10:09:01 +0700

    Update My DB

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const router = require('./routes');
+const db = require('./models');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(router);
-
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-    });
-
+});
