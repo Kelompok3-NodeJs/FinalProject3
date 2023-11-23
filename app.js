cd422fea1b9ba421dc0dffda800bd20c9cd5ac60
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-23 11:51:23 +0700

    Update

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+require("dotenv").config()
+process.env.NODE_ENV = 'development';
+process.env.DATABASE_URL = 'postgresql://postgres:AcgG43*eDdce6g3E*d6ddFe4d2c4C144@monorail.proxy.rlwy.net:11456/railway';
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const router = require('./routes');
 
-const router = require('./routes/index');
-const db = require('./models');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(router);
 
-module.exports = app
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    });
+
