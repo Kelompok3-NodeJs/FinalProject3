8c349f25c4f66470c93f8fff7868172b7d75740c
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-23 11:23:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(router);
-db.sequelize.sync({force: true}).then(() => {
-    console.log("Drop and re-sync db.");
-  });
-
 
 module.exports = app
