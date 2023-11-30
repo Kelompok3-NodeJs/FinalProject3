cc7af1b7bdb7a680af4ad8cdb7a7d376c43ced4a
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-30 20:27:58 +0700

    .

--- models/index.js
+++ models/index.js
@@ -3,11 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-require('dotenv').config(); // Load environment variables from .env file
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
