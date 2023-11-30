cf2cabdb59719f41ca87843795de207232a23c3b
hasanganteng123 <91241087+hasanganteng123@users.noreply.github.com>
2023-11-30 20:37:50 +0700

    .

--- models/index.js
+++ models/index.js
@@ -10,8 +10,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.database_url) {
-  sequelize = new Sequelize(process.env[config.database_url], config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
